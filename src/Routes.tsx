5e9b4bda64aa57701a2a1ce255faaf547c0ff7f9
Pushpanjali Pawar <pushpanjalip55@gmail.com>
2018-11-15 15:16:54 +0100

    Created Challenges Page

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,13 +1,14 @@
 import * as React from "react";
 import { Route, Switch } from "react-router-dom";
 
+import Challenges from "./pages/Challenges/Challenges";
 import Login from "./pages/Login/Login";
-
 const ReactRouter = () => {
   return (
     <React.Fragment>
       <Switch>
         <Route exact={true} path="/login" component={Login} />
+        <Route exact={true} path="/challenges" component={Challenges} />
       </Switch>
     </React.Fragment>
   );
---
+++ src/pages/Challenges/Challenges.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import ChallengesGrid from "../../components/common/CardGrid/CardGrid";
+
+function challenges() {
+  return (
+    <div>
+      <ChallengesGrid />
+    </div>
+  );
+}
+export default challenges;
