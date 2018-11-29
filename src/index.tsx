f0f9d20260dd4c81a6d7467eb73251f10e447713
Tom Klaver <tomklav@gmail.com>
2018-11-29 16:31:28 +0100

    run app under /new

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import App from "./App";
 import "./index.css";
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/new">
     <App />
   </BrowserRouter>,
   document.getElementById("root") as HTMLElement
