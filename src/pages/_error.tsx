006159597348a3f7d26b1bc9b49c0bf1c3c6e953
Tom Klaver <tomklav@gmail.com>
2019-04-30 17:35:54 +0200

    error impl

--- src/pages/_error.tsx
+++ src/pages/_error.tsx
@@ -1,4 +1,12 @@
-export default (props: any) => {
-  console.log('error', props);
-  return <div>OOPS ERROR {JSON.stringify(props)}</div>;
-};
+import React from 'react';
+import { NextContext } from 'next';
+
+export default class extends React.Component {
+  static async getInitialProps(ctx: NextContext) {
+    console.log(ctx);
+    return { ...ctx, req: null, res: null };
+  }
+  render() {
+    return <div>OOPS ERROR {JSON.stringify(this.props)}</div>;
+  }
+}
