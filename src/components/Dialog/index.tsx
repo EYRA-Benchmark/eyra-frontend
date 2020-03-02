6c2cb853606ed2cbc162b6d8bed9817e28471bc6
Pushpanjali Pawar <pushpanjalip55@gmail.com>
2020-03-02 15:06:27 +0100

    Added maxWidth property to reusable Dialog component

--- src/components/Dialog/index.tsx
+++ src/components/Dialog/index.tsx
@@ -2,67 +2,69 @@ import React, { ReactNode } from 'react';
 import CloseIcon from '@material-ui/icons/CloseRounded';
 import PrintIcon from '@material-ui/icons/Print';
 import {
-    DialogActions,
-    Dialog,
-    DialogTitle,
-    DialogContent,
+  DialogActions,
+  Dialog,
+  DialogTitle,
+  DialogContent
 } from '@material-ui/core';
 
 import Slide from '@material-ui/core/Slide';
 import styles from './styles.css';
 interface IProps {
-    title: string;
-    print: boolean;
-    children: ReactNode;
-    onClose: () => any | undefined;
+  title: string;
+  print: boolean;
+  children: ReactNode;
+  maxWidth?: any;
+  onClose: () => any | undefined;
 }
 
 // tslint:disable-next-line:no-shadowed-variable
 const Transition = React.forwardRef(function Transition(props, ref) {
-    // @ts-ignore
-    return <Slide direction="up" ref={ref} {...props} />;
+  // @ts-ignore
+  return <Slide direction="up" ref={ref} {...props} />;
 });
 
 const VisualizationDialog = (props: IProps) => {
-    const handleClose = () => {
-        if (props.onClose) {
-            props.onClose();
-        }
-    };
-    return (
-        <Dialog
-            fullWidth={true}
-            maxWidth={'lg'}
-            open={true}
-            TransitionComponent={Transition as any}
-            keepMounted={true}
-            aria-labelledby="alert-dialog-slide-title"
-            aria-describedby="alert-dialog-slide-description"
+  const handleClose = () => {
+    if (props.onClose) {
+      props.onClose();
+    }
+  };
+  return (
+    <Dialog
+      fullWidth={true}
+      maxWidth={props.maxWidth || 'lg'}
+      open={true}
+      TransitionComponent={Transition as any}
+      keepMounted={true}
+      aria-labelledby="alert-dialog-slide-title"
+      aria-describedby="alert-dialog-slide-description"
+    >
+      <div style={{ display: 'flex' }}>
+        <DialogTitle
+          id="alert-dialog-slide-title"
+          style={{ flex: 5, textAlign: 'center' }}
         >
-            <div style={{ display: 'flex' }}>
-                <DialogTitle id="alert-dialog-slide-title" style={{ flex: 5, textAlign: 'center' }}>
-                    {props.title}
-                </DialogTitle>
-                <DialogActions className={styles.hide} style={{ flex: 1 }}>
-                    {props.print &&
-                        <PrintIcon
-                            onClick={() => window.print()}
-                            color="primary"
-                            style={{ cursor: 'pointer' }}
-                        />}
-                    <CloseIcon
-                        onClick={handleClose}
-                        color="primary"
-                        style={{ cursor: 'pointer' }}
-                    />
-                </DialogActions>
-            </div>
-            <DialogContent >
-                {props.children}
-            </DialogContent>
-
-        </Dialog>
-    );
+          {props.title}
+        </DialogTitle>
+        <DialogActions className={styles.hide} style={{ flex: 1 }}>
+          {props.print && (
+            <PrintIcon
+              onClick={() => window.print()}
+              color="primary"
+              style={{ cursor: 'pointer' }}
+            />
+          )}
+          <CloseIcon
+            onClick={handleClose}
+            color="primary"
+            style={{ cursor: 'pointer' }}
+          />
+        </DialogActions>
+      </div>
+      <DialogContent>{props.children}</DialogContent>
+    </Dialog>
+  );
 };
 
 export default VisualizationDialog;
