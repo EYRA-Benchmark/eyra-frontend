385ae96776d1027ee23123adb02683b372a34286
Pushpanjali Pawar <pushpanjalip55@gmail.com>
2020-11-04 19:31:04 +0100

    Created news grid component

---
+++ src/components/NewsGallery/NewsGrid.tsx
@@ -0,0 +1,33 @@
+import * as React from 'react';
+
+import { Grid } from '@material-ui/core';
+import { IBenchmark } from 'src/types';
+
+import { BenchmarkCard } from 'src/components/BenchmarkCard';
+
+interface IProps {
+  size: number;
+  benchmarks: IBenchmark[];
+}
+
+const sortByDate = (a: IBenchmark, b: IBenchmark) =>
+  new Date(b.modified).getTime() - new Date(a.modified).getTime();
+
+export class NewsGrid extends React.Component<IProps, {}> {
+  render() {
+    const { size, benchmarks } = this.props;
+    const filteredBenchmarks = benchmarks
+      .sort(sortByDate)
+      .slice(0, size > 0 ? size : benchmarks.length);
+    return (
+      <Grid container={true} spacing={3}>
+        {filteredBenchmarks.map((benchmark, index: number) => (
+          <Grid item={true} key={index} xs={12} sm={3} md={3}>
+            <BenchmarkCard benchmark={benchmark} />
+          </Grid>
+        ))}
+      </Grid>
+    );
+  }
+}
+export default CardGrid;
